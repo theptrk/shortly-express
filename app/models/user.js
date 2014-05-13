fc7a336b44b3c3caad259f11c14fc46bbd491d1e
Hack Reactor Students <patrick.tran06@gmail.com>
2014-05-12 23:19:35 -0700

    Move hashing logic out of initialize to avoid issues with new.

--- app/models/user.js
+++ app/models/user.js
@@ -3,6 +3,9 @@ var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 var Link = require('./link');
 
+var promiseHash = Promise.promisify(bcrypt.hash);
+var promiseSalt = Promise.promisify(bcrypt.genSalt);
+
 var User = db.Model.extend({
   tableName: 'users',
   hasTimestamps: true,
@@ -11,13 +14,14 @@ var User = db.Model.extend({
     return this.hasMany(Link);
   },
 
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      // var shasum = crypto.createHash('sha1');
-      // shasum.update(model.get('url'));
-      // model.set('code', shasum.digest('hex').slice(0, 5));
-    });
+  hashStore: function() {
+    return promiseHash(this.get('password'), null, null)
+      .then(function(hash) {
+        this.set('password', hash);
+        return this.save();
+      }.bind(this));
   }
+
 });
 
 module.exports = User;
